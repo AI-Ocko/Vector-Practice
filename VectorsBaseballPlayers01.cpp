0dd4fd36e16f9db87aa3ab8a3c331a16ab475bb4
christopher <135375609+AI-Ocko@users.noreply.github.com>
2025-04-28 19:41:23 -0700

    Created boiler plate added comments with vector commands

---
+++ VectorsBaseballPlayers01.cpp
@@ -0,0 +1,30 @@
+/*
+vecList[index] returns the element at the position specified by index. 
+vecList.front() retrun the first element
+vecList.back() return the last element
+vecList.clear() deletes all elements from the object
+vecList.push_back(elem) a copy of elem is inserted into vecList at the end
+vecList.pop_back() delete the last element of vecList
+vecList.empty() return true if the object vecList is empty and false otherwise.
+vecList.size() return the number of elements currently in the object vecList
+vecList.max_size() return the max number of elements that can be inserted into the object. 
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main () {
+    //declarations
+    
+    //get user input
+
+    //calculations
+
+    //output
+
+    system ("pause");
+    return 0;
+}
