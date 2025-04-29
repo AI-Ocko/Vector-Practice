9db99727f64da03f283aa34fca4aa12d41e02dbe
christopher <135375609+AI-Ocko@users.noreply.github.com>
2025-04-28 20:00:13 -0700

    wrote the entrie program

--- VectorsBaseballPlayers01.cpp
+++ VectorsBaseballPlayers01.cpp
@@ -18,12 +18,62 @@ using namespace std;
 
 int main () {
     //declarations
-    
+    vector<int> playerNumbers;
+    vector<string> playerNames;
+    vector<bool> playerAvailable;
+
+    int numOfPlayers;
+    int countPlayers;
+    int num;
+    string name;
+    char playingToday = 'N';
+    bool isAvailable;
+    bool isNotAvailable = true;
+
     //get user input
+    cout << "How many players are on the team? ";
+    cin >> numOfPlayers;
 
     //calculations
+    for (countPlayers = 0; countPlayers < numOfPlayers; countPlayers++)
+    {
+        cout << "What is player " << countPlayers + 1 << "'s name?: ";
+        cin >> name;
+        playerNames.push_back(name);
+
+        cout << "What is player " << playerNames[countPlayers] << "'s number?: ";
+        cin >> num;
+        playerNumbers.push_back(num);
+
+        cout << "Is " << playerNames[countPlayers] << " playing today? (Y/N): ";
+        cin >> playingToday;
+
+        if (playingToday == 'Y' || playingToday == 'y') 
+        {
+            playerAvailable.push_back(true);
+        }
+        else
+        {
+            playerAvailable.push_back(false);
+        }
+    }
 
     //output
+    for (countPlayers = 0; countPlayers < numOfPlayers; countPlayers++)
+    {
+        cout << playerNames[countPlayers] << "'s jersey number is " << playerNumbers[countPlayers] << " and he is ";
+
+        if (playerAvailable[countPlayers] == true)
+        {
+            cout << "playing today.";
+        }
+        else 
+        {
+            cout << "not playing today.";
+        }
+
+        cout << endl;
+    }
 
     system ("pause");
     return 0;
